e528740ac45fdbd8568d242e3c9d89c1234f7d8e
haramari <haramari.rps@gmail.com>
2020-01-03 21:57:19 +0900

    change the top image

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ !empty($pagetitle) ? $pagetitle : config('app.name', 'Portforio') }}</title>
+
+  <!-- Styles -->
+  <!--
+      <link href="{{ asset('css/app.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/layout.min.css') }}" rel="stylesheet">
+  -->
+  @yield('css')
+  
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  @yield('javascript')
+</head>
+<body>
+  <div id="app">
+    <header class="">
+    </header>
+
+    <main class="py-4">
+      <div class='container'>
+        @include('layouts.flash-messages')
+        @yield('content')
+      </div>
+    </main>
+  </div>
+    
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,40 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Portforio</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- CSS -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+        <!-- JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
         <!-- Styles -->
         <style>
+
+        @font-face {
+            font-family: 'Bonbon-Regular';
+            src: url('{{asset('/fonts/Bonbon-Regular.ttf')}}');
+        }
+
             html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                background-color: #FEF2DA;
+                background-image: url(''); 
+                background-size: auto;
+                color: #94654C;
                 font-weight: 200;
                 height: 100vh;
-                margin: 0;
+                margin: 0;                
+            }
+
+            .text {
+                font-family: 'Bonbon-regular';
             }
 
             .full-height {
@@ -48,19 +68,11 @@
                 font-size: 84px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
             .m-b-md {
                 margin-bottom: 30px;
+                font-family: 'Bonbon-regular';
             }
+
         </style>
     </head>
     <body>
@@ -68,12 +80,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}"><button type="button" class="btn btn-light">Home</button></a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}"><button type="button" class="btn btn-light">Login</button></a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}"><button type="button" class="btn btn-light">Register</button></a>
                         @endif
                     @endauth
                 </div>
@@ -81,20 +93,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Haruto Aki
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                <img src="images/bambi_l.jpg" alt="Avatar" class="image">
+                  <div class="overlay">
+                    <div class="text"><h2>Enter</h2></div>
+                  </div>
+
         </div>
     </body>
 </html>
